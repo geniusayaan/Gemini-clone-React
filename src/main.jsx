846e98a73d53c3120e2775ecc732a2b5d198eab6
SHAKIR <shakirsjd@gmail.com>
2024-11-16 18:17:31 +0530

    finlly going to deploy

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {BrowserRouter} from "react-router-dom"
-import ContextProvider from './context/context.jsx'
+import ContextProvider from './context/Context.jsx'
 
 createRoot(document.getElementById('root')).render(
   
